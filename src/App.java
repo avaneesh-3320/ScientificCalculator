876429ad0f87477f59ed304044ee197486511c38
Avaneesh <90546562+avaneesh-3320@users.noreply.github.com>
2025-03-08 12:58:33 +0000

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,13 +1,1 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        for(int i = 1; i <= 12; i++){
-            int sigma = 0;
-            int factorial = 1;
-            for(int j = 1; j <= i; j++){
-                sigma += j;
-                factorial *= j;
-            }
-            System.out.println(i + "=" + sigma + "and fact = " + factorial);
-        }
-    }
-}
+
